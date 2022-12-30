1f2227af7255cfb8f97898f24aafe61029c00d73
Garrett <lamgarr@outlook.com>
2022-12-29 21:51:12 -0800

    Add login page and styling

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/Login.css';
 
 function Login(){
     return(
         <body>
-            login
+            <div className='content'>
+                <h1>Login</h1>
+                <form>
+                    <input required type='text' placeholder='Username'/>
+                    <input required type='password' placeholder='Password'/>
+                    <div className='help'>
+                        Forgot Pawword
+                    </div>
+                </form>
+                <button>Log In</button>
+                <div className='members'>
+                Not a member? <Link to='/sign-up'>Sign Up</Link>
+                </div>
+            </div>
         </body>
     )
 }
