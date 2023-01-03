40b56556fb75863af41e612dac9bd5f170677ed4
Garrett <lamgarr@outlook.com>
2023-01-02 21:12:07 -0800

    Add styling for input errors

--- client/src/components/Input.tsx
+++ client/src/components/Input.tsx
@@ -18,7 +18,10 @@ function Input(prop:propHandler){
 
     return(
         <div className={`inputField ${input !==''?'field_not_empty':''}`}>
-            <label className='fieldLabel' htmlFor={prop.type}>{prop.placeholder.toLowerCase()}</label>
+            <div className='fieldLabel'>
+            <label htmlFor={prop.type}>{prop.placeholder.toLowerCase()}</label>
+            <span className='err'>error</span>
+            </div>
             <input className='fieldInput'id={prop.type} placeholder={prop.placeholder} type={prop.type} value={input} onChange={changeInput}></input>
         </div>
     )
