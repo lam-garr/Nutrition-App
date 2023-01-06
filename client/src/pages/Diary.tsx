4010c9dcbc76e17e2cc6cc3abdf7f5fbb64dd0fc
Garrett <lamgarr@outlook.com>
2023-01-05 21:44:40 -0800

    Add and edit elements

--- client/src/pages/Diary.tsx
+++ client/src/pages/Diary.tsx
@@ -13,15 +13,35 @@ function Diary(){
                         <span className='date-day'>5</span>
                         <span>2023</span>
                     </div>
-                    <div className='sec-one-buttons'>
+                    <div className='sec-one-right'>
+                        <div className='sec-one-stats'>
+                            <div>10</div>
+                            <div>20</div>
+                            <div>30</div>
+                            <div>2000</div>
+                        </div>
+                        <div className='sec-one-buttons'>
                         <button>save</button>
                         <button>add food</button>
+                        </div>
+                        <button className='content-btn'>?</button>
                     </div>
                 </div>
             </section>
             <section className='food-diary-section-two'>
             <div className='section-two-content'>
-                    p2
+                    <table>
+                        <tr>
+                            <th>ex. 1</th>
+                            <th>ex. 2</th>
+                            <th>ex. 3</th>
+                        </tr>
+                        <tr>
+                            <td>example for ex. 1</td>
+                            <td>example for ex. 2</td>
+                            <td>example for ex. 3</td>
+                        </tr>
+                    </table>
                 </div>
             </section>
         </main>
