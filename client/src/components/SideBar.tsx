c37a850b8bb39ba0b67b68c50b728e8afef3862e
Garrett <lamgarr@outlook.com>
2023-01-15 19:36:14 -0800

    Add sidebar component

---
+++ client/src/components/SideBar.tsx
@@ -0,0 +1,27 @@
+import React, {useRef}from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/SideBar.css';
+
+interface propInterface{
+    clickHandler: (event: React.MouseEvent<HTMLButtonElement>) => void,
+    closeHandler: () => void,
+    sideBarOpen: boolean;
+}
+
+function SideBar(prop: propInterface){
+
+    //check local storage to see if user is authenticated with access token
+    const loggenIn = true;
+
+    return(
+        <aside className={`sidebar ${prop.sideBarOpen?'active':'inactive'}`}>
+            <button className='sidebar-close' onClick={prop.clickHandler}></button>
+            <Link to={'/'}><button onClick={prop.closeHandler}>Home</button></Link>
+            <Link to={'/diary'}><button onClick={prop.closeHandler}>Food Diary</button></Link>
+            {loggenIn && <Link to={'/user/collection'}><button onClick={prop.closeHandler}>Diary Entries</button></Link>}
+            {loggenIn && <Link to={'/user/account'}><button onClick={prop.closeHandler}>Account</button></Link>}
+        </aside>
+    )
+}
+
+export default SideBar;
