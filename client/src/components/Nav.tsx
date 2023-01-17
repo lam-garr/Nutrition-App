2030b09ec12aad7ae3ea098d4f663dd24d97a0dd
Garrett <lamgarr@outlook.com>
2023-01-16 19:28:28 -0800

    Fix additional styling

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -10,15 +10,21 @@ function Nav(prop: propInterface){
     return(
         <nav className='nav-bar'>
             <div className='nav-container'>
-                <button onClick={prop.sidebarHandler}>menu</button>
+                <button className='menu-btn' onClick={prop.sidebarHandler}>
+                    <div className='menu-div'></div>
+                    <div className='menu-div'></div>
+                    <div className='menu-div'></div>
+                </button> 
                 <div className='nav-logo'>
                     <Link to={'/'}>
-                        <h1>Title</h1>
+                        <h1>NutriLog</h1>
                     </Link>
                 </div>
+                <div></div>
             </div>
         </nav>
     )
 }
 
 export default Nav;
+
