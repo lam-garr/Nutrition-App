e7ec6101347bceb7a255b6e485c2831f2584e9c7
Garrett <lamgarr@outlook.com>
2023-01-24 18:49:48 -0800

    Add route and controller for sign up

--- server/controllers/apiController.ts
+++ server/controllers/apiController.ts
@@ -3,10 +3,29 @@ import User from '../models/user';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import axios from 'axios';
+import { nextTick } from 'process';
 
 //function for index api call
 export function GET_index(req: Request, res: Response){
     res.json({message:'Welcome!!!'});
+}
+
+//function for user sign up api call
+export async function POST_sign_up(req: Request, res: Response, next: NextFunction){
+    const hashedPw = await bcrypt.hash(req.body.password, 10);
+
+    const user = new User({
+        username: req.body.username,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        password: hashedPw
+    }).save(err => {
+        if(err){
+            return next(err);
+        }else{
+            res.json({message:'Success'});
+        }
+    })
 }
 
 //make api call to edamam
--- server/routes/apiRoutes.ts
+++ server/routes/apiRoutes.ts
@@ -1,10 +1,12 @@
 import express from 'express';
-import { GET_index, GET_NUTR_info } from '../controllers/apiController';
+import { GET_index, GET_NUTR_info, POST_sign_up } from '../controllers/apiController';
 
 const router = express.Router();
 
-router.get('/', GET_index)
+router.get('/', GET_index);
 
-router.get('/nutr', GET_NUTR_info)
+router.get('/nutr', GET_NUTR_info);
+
+router.post('/sign-up', POST_sign_up);
 
 export default router;
