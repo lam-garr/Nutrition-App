d23539f36dc5ff407e092bf677476b250a60d302
Garrett <lamgarr@outlook.com>
2023-03-07 21:41:49 -0800

    Edit home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import berryImg from '../utils/imgs/img1.jpg';
+import saladImg from '../utils/imgs/img2.jpg';
+import avacadoImg from '../utils/imgs/img3.jpg';
 import '../styles/Home.css'
 
 function Home(){
@@ -9,38 +12,36 @@ function Home(){
             <header className='header'>
                 <div className='container'>
                     <div>
-                        <h1>Nutrition Stuff</h1>
-                        <p>nutrition info stuff etc, etc.</p>
-                        {/*Edit Link to with actual path!!!!!*/}
-                        <Link to={"/sign-up"}><button type='button'>Start Now!, or not</button></Link>
+                        <h1>A healthy life starts with what you eat.</h1>
+                        <p>Track meals and nutrients to raech your goals with NutriLog.</p>
+                        <Link to={"/sign-up"}><button type='button'>Start For Free</button></Link>
                     </div>
-                    {/*Add src for photo and edit alt desc*/}
-                    <img src='' alt='sample pic'/>
+                    <img src={saladImg} alt='sample pic'/>
                 </div>
             </header>
             <section className='section-one'>
                 <div className='container'>
                     {/*Add src for photo and edit alt desc*/}
-                    <img src='' alt='sample pic'/>
+                    <img src={berryImg} alt='sample pic'/>
                     <div>
-                        <h1>More Nutrition Stuff..</h1>
-                        <p>etc, etc, etc..</p>
+                        <h1>Log from our large selection of food items.</h1>
+                        <p>View a breakdown of calories and nutrients from your favorites items.</p>
                     </div>
                 </div>
             </section>
             <section className='section-two'>
                 <div className='container'>
                     <div className='box'>
-                        <h2>Info...</h2>
-                        <p>stuf, stuff, stuff...</p>
+                        <h2>Track and Learn</h2>
+                        <p>Keep track of your progress and adapt.</p>
                     </div>
                     <div className='box'>
-                        <h2>Info...</h2>
-                        <p>stuf, stuff, stuff...</p>
+                        <h2>Simplified Logging</h2>
+                        <p>Enter your favorite foods and brands.</p>
                     </div>
                     <div className='box'>
-                        <h2>Info...</h2>
-                        <p>stuf, stuff, stuff...</p>
+                        <h2>Free To Use</h2>
+                        <p>Log from our large selection of items for free.</p>
                     </div>
                 </div>
             </section>
